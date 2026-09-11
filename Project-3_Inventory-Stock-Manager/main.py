47d8d1eafdebc3d79a39b60478871141020d07b9
kumar nallana <sasikumarnallana956@gmail.com>
2026-09-11 10:25:49 +0530

    feat: add low stock product filtering

--- Project-3_Inventory-Stock-Manager/main.py
+++ Project-3_Inventory-Stock-Manager/main.py
@@ -10,7 +10,13 @@ if __name__ == "__main__":
     try:
         inventory_stock_manager = InventoryStockManager(
             products_db=products_data)
-        low_stock_products = inventory_stock_manager.find_low_stock(2)
-        pprint(low_stock_products, indent=4)
+
+        # FINDING LOW STOCK PRODUCTS
+        # low_stock_products = inventory_stock_manager.find_low_stock(2)
+        # pprint(low_stock_products, indent=4)
+
+        # FIND PRODUCT BY id
+        products_by_id = inventory_stock_manager.find_product_by_id(1012)
+        pprint(products_by_id, indent=4)
     except Exception as e:
         print(f"⚠️ Error Occured: {e}")
